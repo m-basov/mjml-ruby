8b07143ed868da875e61ad2812909665f37b4a08
Mykola Basov <kolybasov@yandex.ru>
2016-09-06 20:25:59 +0300

    Refactor ActionView template handler

---
+++ lib/mjml/rails/template_handler.rb
@@ -0,0 +1,29 @@
+module MJML
+  module Rails
+    # MJML Template handler for Rails
+    class TemplateHandler
+      # Supported extensions
+      EXTENSIONS = Hash[erb: :mjml, slim: :mjmlslim, haml: :mjmlhaml]
+
+      def initialize(base_handler = :erb)
+        @base_handler = base_handler
+      end
+
+      def call(template)
+        compiled = get_handler(@base_handler).call(template)
+        "::MJML::Parser.new.call(begin;#{compiled};end).html_safe"
+      end
+
+      private
+
+      def get_handler(ext)
+        handler = instance_variable_get("@#{ext}_handler")
+        if handler.nil?
+          handler = ActionView::Template.registered_template_handler(ext)
+          instance_variable_set("@#{ext}_handler", handler)
+        end
+        handler
+      end
+    end
+  end
+end
--- lib/mjml/railtie.rb
+++ lib/mjml/railtie.rb
@@ -1,36 +1,11 @@
 require 'rails/railtie'
 require 'mjml'
 require 'mjml/parser'
+require 'mjml/rails/template_handler'
 
 module MJML
   # Rails plugin for MJML ruby
   class Railtie < ::Rails::Railtie
-    # Template handler for Rails
-    class Handler
-      def initialize(base_handler = :erb)
-        @base_handler = base_handler
-      end
-
-      def call(template)
-        compiled = send("#{@base_handler}_handler").call(template)
-        "::MJML::Parser.new.call(begin;#{compiled};end).html_safe"
-      end
-
-      private
-
-      def erb_handler
-        @erb_handler ||= ActionView::Template.registered_template_handler(:erb)
-      end
-
-      def slim_handler
-        @slim_handler ||= ActionView::Template.registered_template_handler(:slim)
-      end
-
-      def haml_handler
-        @haml_handler ||= ActionView::Template.registered_template_handler(:haml)
-      end
-    end
-
     # Config
     config.mjml = MJML.config
 
@@ -41,9 +16,9 @@ module MJML
       end
 
       ActiveSupport.on_load(:action_view) do
-        ActionView::Template.register_template_handler(:mjml, Handler.new)
-        ActionView::Template.register_template_handler(:mjmlslim, Handler.new(:slim))
-        ActionView::Template.register_template_handler(:mjmlhaml, Handler.new(:haml))
+        MJML::Rails::TemplateHandler::EXTENSIONS.each do |h, ext|
+          ActionView::Template.register_template_handler(ext, MJML::Rails::TemplateHandler.new(h))
+        end
       end
     end
   end
