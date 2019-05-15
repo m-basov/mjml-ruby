6ebb96243be87ae65385b0b33b511118e243031a
Nicolas Brousse <nicolas@pantographe.studio>
2019-05-15 19:41:45 +0200

    Raise better error when mjml binary is not found

---
+++ spec/mjml_spec.rb
@@ -0,0 +1,20 @@
+require_relative 'spec_helper'
+require 'mjml'
+
+describe 'MJML' do
+  describe '#extract_executable_version' do
+    it 'should return the version if mjml is installed' do
+      MJML.extract_executable_version.must_be_instance_of String
+    end
+
+    it 'should return false for unknown feature' do
+      bin_path = MJML.config.bin_path
+      MJML.config.bin_path = '/usr/bin/env mjml-not-installed'
+
+      error = -> { MJML.extract_executable_version }.must_raise MJML::BinaryNotFound
+      error.message.must_match(/^mjml binary not found/)
+
+      MJML.config.bin_path = bin_path
+    end
+  end
+end
